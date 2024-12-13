319805b40dea7f1b86987368ad6743fabdfa58f4
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-13 18:15:59 +0300

    Update color palette and add dark/light mode support

--- app/src/main/java/com/example/profilecard/ui/theme/Color.kt
+++ app/src/main/java/com/example/profilecard/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.example.profilecard.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Kendi eklediğim renkler
+val FirstColor = Color(0xFF0090FF)
+val FirstTextColor = Color(0xFF000000)
+val SecondTextColor = Color(0xFFA19C9C)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkFirstColor = Color(0xFF0041C2)
+val DarkFirstTextColor = Color(0xFFFFFFFF)
+val DarkSecondTextColor = Color(0xFF5F5E5E)
