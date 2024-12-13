32177e1333e09b2c3a2d7a1674f67244cac42b5a
İsmail Can Varlı <ismailcanvarli@gmail.com>
2024-12-13 18:17:20 +0300

    Add profile image and update design

--- app/src/main/java/com/example/profilecard/MainActivity.kt
+++ app/src/main/java/com/example/profilecard/MainActivity.kt
@@ -3,18 +3,25 @@ package com.example.profilecard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.profilecard.ui.theme.ProfileCardTheme
@@ -22,7 +29,6 @@ import com.example.profilecard.ui.theme.ProfileCardTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ProfileCardTheme {
                 ProfileCardScreen()
@@ -33,62 +39,77 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProfileCardScreen() {
-    Scaffold(topBar = {
-        CenterAlignedTopAppBar(
-            title = { Text(text = "Profile Card") },
-
+fun ProfileCardScreen(darkTheme: Boolean = isSystemInDarkTheme()) {
+    ProfileCardTheme(darkTheme = darkTheme) {
+        Scaffold(topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = stringResource(R.string.profile_card)) },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary
+                )
             )
-    }) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Column {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = "Profile Picture",
-                    modifier = Modifier.padding(16.dp)
+        }) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+            ) {
+                ProfileImageAndName()
+                ProfileDetailsRow(
+                    label = stringResource(R.string.name),
+                    value = stringResource(R.string.name_detail)
                 )
-
-                Text(
-                    text = "John Doe", modifier = Modifier.padding(16.dp)
+                ProfileDetailsRow(
+                    label = stringResource(R.string.ability),
+                    value = stringResource(R.string.ability_detail)
                 )
-            }
-
-            Column {
-                Row {
-                    Text(
-                        text = "Email", modifier = Modifier.padding(16.dp)
-                    )
-                    Text(
-                        text = "asd", modifier = Modifier.padding(16.dp)
-                    )
-                }
-                Row {
-                    Text(
-                        text = "Linkedln", modifier = Modifier.padding(16.dp)
-                    )
-                    Text(
-                        text = "asd", modifier = Modifier.padding(16.dp)
-                    )
-                }
-                Row {
-                    Text(
-                        text = "Github", modifier = Modifier.padding(16.dp)
-                    )
-                    Text(
-                        text = "asd", modifier = Modifier.padding(16.dp)
-                    )
-                }
-                Row {
-                    Text(
-                        text = "Web Site", modifier = Modifier.padding(16.dp)
-                    )
-                    Text(
-                        text = "asd", modifier = Modifier.padding(16.dp)
-                    )
-                }
+                ProfileDetailsRow(
+                    label = stringResource(R.string.team),
+                    value = stringResource(R.string.team_detail)
+                )
+                ProfileDetailsRow(
+                    label = stringResource(R.string.power),
+                    value = stringResource(R.string.power_detail)
+                )
             }
         }
+    }
+}
 
+@Composable
+fun ProfileImageAndName() {
+    Column(
+        modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.hero_profile_photo),
+            contentDescription = stringResource(R.string.profile_picture),
+            modifier = Modifier
+                .padding(start = 8.dp, top = 8.dp, end = 8.dp)
+                .size(250.dp)
+        )
+        Text(
+            text = stringResource(R.string.iron_man),
+            color = MaterialTheme.colorScheme.onPrimary,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
+@Composable
+fun ProfileDetailsRow(label: String, value: String) {
+    Row {
+        Text(
+            text = label,
+            color = MaterialTheme.colorScheme.secondary,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = value,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(16.dp)
+        )
     }
 }
 
